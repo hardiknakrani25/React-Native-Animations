71fc2983e2e58a8385f1630610a8c3cc4f9c1845
Hardik <hdnakarani@gmail.com>
2019-04-05 19:54:36 +0530

    set position and animation in whole card list

--- src/Deck.js
+++ src/Deck.js
@@ -4,8 +4,17 @@ import { View, Animated, PanResponder } from "react-native";
 class Deck extends Component {
   constructor(props) {
     super(props);
-    const panResponder = PanResponder.create({});
-    this.state = { panResponder };
+
+    const position = new Animated.ValueXY();
+
+    const panResponder = PanResponder.create({
+      onStartShouldSetPanResponder: () => true,
+      onPanResponderMove: (event, gesture) => {
+        position.setValue({ x: gesture.dx, y: gesture.dy });
+      },
+      onPanResponderRelease: () => {}
+    });
+    this.state = { panResponder, position };
   }
   renderCards() {
     return this.props.data.map(item => {
@@ -14,7 +23,14 @@ class Deck extends Component {
   }
 
   render() {
-    return <View>{this.renderCards()}</View>;
+    return (
+      <Animated.View
+        style={this.state.position.getLayout()}
+        {...this.state.panResponder.panHandlers}
+      >
+        {this.renderCards()}
+      </Animated.View>
+    );
   }
 }
 
